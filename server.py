f8a411010d7aeb77d2fc1cb7da2c44dfd649de54
Shachar David <shachadavid2013@gmail.com>
2023-05-12 19:05:02 +0300

    started adding permissions to server

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException, Depends, dependencies, Path, Request
+from typing import Annotated
+import uvicorn
 from models.commands import Commands
+import string
 
 app = FastAPI()
 
 commands = Commands()
+
+
+async def ip_permissions(request: Request):
+
+
+@app.get('/admin')
+async def admin_page():
+    return
 
 
 @app.get('/commands')
@@ -13,5 +23,9 @@ async def get_commands():
 
 
 @app.get('/commands/{name}')
-async def run_command(name, params):
+async def run_command(name: Annotated[str, Path(title="the name of the command to run")], params):
     return commands.run_command(name, eval(params))
+
+
+if __name__ == '__main__':
+    uvicorn.run(host='127.0.0.1', port=5461, reload=True, app='server:app')
