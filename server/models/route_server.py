4c372f4e35839f27f9ce31ab8f38f646ae469ed8
Shachar David <shachadavid2013@gmail.com>
2023-07-14 13:17:36 +0300

    created route object

--- server/models/route_server.py
+++ server/models/route_server.py
@@ -1,12 +1,23 @@
-from typing import Tuple
+from typing import Tuple, Callable
 
 from server.models.packet import Response, Request
 from server.models.tcp_server import TCPServer
 
 
+class Route:
+    def __init__(self, path: str, args: list, action: Callable):
+        self.path = path
+        self.args = args
+        self.action = action
+
+    def run(self, *args):
+        self.action(args)
+
+
 class RouteServer(TCPServer):
     def __init__(self, address: Tuple[str, int]):
         super().__init__(address)
+        self.routes =
 
     def handle_request(self, request: Request) -> Response:
         pass
