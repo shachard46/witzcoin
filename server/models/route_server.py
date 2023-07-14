7d22cd50bd46e26b02d470fc01e4f28682e80b77
Shachar David <shachadavid2013@gmail.com>
2023-07-14 13:43:02 +0300

    temp finish routeserver

--- server/models/route_server.py
+++ server/models/route_server.py
@@ -1,4 +1,5 @@
-from typing import Tuple, Callable
+import inspect
+from typing import Tuple, Callable, List
 
 from server.models.packet import Response, Request
 from server.models.tcp_server import TCPServer
@@ -10,17 +11,35 @@ class Route:
         self.args = args
         self.action = action
 
-    def run(self, *args):
-        self.action(args)
+    def run(self, *args) -> Tuple[str, dict]:
+        return self.path, self.action(args)
 
 
 class RouteServer(TCPServer):
     def __init__(self, address: Tuple[str, int]):
         super().__init__(address)
-        self.routes =
+        self.routes: List[Route] = []
+
+    def add_route(self, path: str, action: Callable):
+        args = list(inspect.signature(action).parameters.keys())
+        route = Route(path, args, action)
+        self.routes.append(route)
+
+    def route(self, path: str):
+        def decorator(func):
+            self.add_route(path, func)
+            return func
+
+        return decorator
+
+    def find_route(self, path: str):
+        for route in self.routes:
+            if route.path == path:
+                return route
+        return None
 
     def handle_request(self, request: Request) -> Response:
-        pass
-
-    def handle_response(self, response: Response):
-        pass
+        route = self.find_route(request.path)
+        if not route:
+            return Response(request.path, {'error': 'no such route'})
+        return Response(*route.run(request.payload))
