8a786e8d7565b25b2ad1fec7de7de8145ba3ef1a
Shachar David <shachadavid2013@gmail.com>
2023-07-14 16:37:20 +0300

    routes actions are done, only tcp encoding left

--- server/target_server.py
+++ server/target_server.py
@@ -1,26 +1,28 @@
 from server.models.commands import Commands
 from server.models.route_server import RouteServer
-from server.models.tcp_client import TCPClient
 
 app = RouteServer(host='127.0.0.1', port=5461)
 commands = Commands()
 
 
-@app.route('run/{command}')
-def run_command(command: str, params: dict):
-    pass
+@app.route('run/{alias}')
+def run_command(alias: str, params: dict):
+    return commands.run_command(alias, params)
 
 
 @app.route('get')
 def get_all_commands():
-    pass
+    return commands.get_all_commands()
 
 
-@app.route('get/{command}')
-def get_command(command: str):
-    pass
+@app.route('get/{alias}')
+def get_command(alias: str):
+    return commands.get_command(alias)
 
 
-@app.route('delete/{command}')
-def delete_command(command: str):
-    pass
+@app.route('delete/{alias}')
+def delete_command(alias: str):
+    try:
+        return commands.remove_by_alias(alias)
+    except:
+        return 'no such flower'
