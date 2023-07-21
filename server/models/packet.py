a01613cf3243ff5e749f1f22717d9bc9eaba91e4
Shachar David <shachadavid2013@gmail.com>
2023-07-21 17:58:58 +0300

    handling long paths by changing extract line

--- server/models/packet.py
+++ server/models/packet.py
@@ -5,6 +5,7 @@ from server import utils
 
 class Packet:
     PACKET_SIZE = 15
+    CODE_SIZE = 4
     codes = {
         'path': '19e5',
         'param_part': '673a',
@@ -16,27 +17,33 @@ class Packet:
         pass
 
     @staticmethod
-    def extract_line_from_raw(line: str, field):
-        if not line.startswith(Packet.codes[field]):
-            return ''
-        return Packet.decode_value(line.replace(Packet.codes[field], '', 1))
+    def _extract_line_from_raw(line: str) -> Tuple:
+        code = line[:Packet.CODE_SIZE]
+        field = ''
+        for f, c in Packet.codes.items():
+            if c == code:
+                field = f
+                break
+        if not field:
+            return '', ''
+        return field, Packet._decode_value(line.replace(code, '', 1))
 
     @staticmethod
-    def join_to_line(code, value, more=True):
-        line = code + Packet.encode_value(value) if more else code + Packet.encode_value(
+    def _join_to_line(code, value, more=True):
+        line = code + Packet._encode_value(value) if more else code + Packet._encode_value(
             value)
-        line += '0' * (Packet.PACKET_SIZE - len(Packet.codes['end_packet']) - len(line))
+        line += '0' * (Packet.PACKET_SIZE - Packet.CODE_SIZE - len(line))
         line += Packet.codes['end_packet'] if more else Packet.codes['pasten']
         return line
 
     @staticmethod
-    def encode_value(value: str) -> str:
+    def _encode_value(value: str) -> str:
         return ''.join([hex(ord(c))[2:] for c in value])
 
     @staticmethod
-    def decode_value(value: str) -> str:
+    def _decode_value(value: str) -> str:
         res = []
-        for i in range(0, len(value) - len(Packet.codes['end_packet']), 2):
+        for i in range(0, len(value) - Packet.CODE_SIZE, 2):
             if value[i: i + 4] == '0000':
                 break
             res.append(chr(int(value[i: i + 2], 16)))
@@ -44,12 +51,14 @@ class Packet:
 
     @staticmethod
     def extract_content(raw: List[str]) -> Tuple[str, dict]:
-        path = Packet.extract_line_from_raw(raw[0], 'path')
-        if len(raw) < 2 or not path:
-            return path, {}
+        path = ''
         payload = []
         for packet in raw:
-            payload.append(Packet.extract_line_from_raw(packet, 'param_part'))
+            field, value = Packet._extract_line_from_raw(packet)
+            if field == 'path':
+                path += value
+            else:
+                payload.append(value)
         try:
             return path, eval(utils.filter_unreadable_chars(''.join(payload)))
         except SyntaxError:
@@ -58,16 +67,16 @@ class Packet:
     @staticmethod
     def encode_content(path: str, payload: dict) -> List[str]:
         more = True if payload else False
-        raw = [Packet.join_to_line(Packet.codes['path'], path, more)]
+        raw = [Packet._join_to_line(Packet.codes['path'], path, more)]
         if not payload:
             return raw
         string_payload = str(payload)
-        content_size = (Packet.PACKET_SIZE - len(Packet.codes['param_part']) * 2) // 2
+        content_size = (Packet.PACKET_SIZE - Packet.CODE_SIZE * 2) // 2
         for start in range(0, len(string_payload), content_size):
             more = start + content_size < len(string_payload)
             end = content_size + start if more else len(string_payload)
             packet_string = string_payload[start: end]
-            raw.append(Packet.join_to_line(Packet.codes['param_part'], packet_string, more))
+            raw.append(Packet._join_to_line(Packet.codes['param_part'], packet_string, more))
         return raw
 
 
