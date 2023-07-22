0dca6f66c9dba628e926414ed52d839f484c5049
Shachar David <shachadavid2013@gmail.com>
2023-07-22 16:44:31 +0300

    only letters in ipsum to avoid dups

--- scramble/get_all_words.py
+++ scramble/get_all_words.py
@@ -28,10 +28,11 @@ def get_all_words(files):
 
 
 def get_lorem_ipsum_words(length):
-    pattern = re.compile('(.*? .*?) ')
+    pattern = re.compile(r'(\w*? \w*?) ')
     # res = requests.get('https://baconipsum.com/api/?type=meat-and-filler&paras=100000')
     with open('lorem.txt') as f:
-        return list(set(pattern.findall(f.read())))[:length]
+        ipsum_words = list(set(pattern.findall(f.read())))
+        return ipsum_words[:length]
 
 
 def main():
