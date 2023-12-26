debe53f4ed3978c08099120a012b3b83af6af813
shachardavid <shachadavid2013@gmail.com>
2023-12-27 00:51:04 +0200

    added status of transaction row, need to add status to transaction interface and think about the structure of the collapse

---
+++ frontend/src/componentes/transaction/transaction-row.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Transaction } from "./models";
+import { Collapse, IconButton, TableCell, TableRow } from "@mui/material";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+
+export const TransactionRow: React.FC<Transaction> = (transaction: Transaction) => {
+      const [open, setOpen] = React.useState(false);
+
+    return <React.Fragment>
+        <TableRow>
+
+            <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+            <TableCell align="center">{transaction.transactionName}</TableCell>
+            <TableCell align="center">{transaction.buyerUsername}</TableCell>
+            <TableCell align="center">{transaction.sellerUsername}</TableCell>
+            <TableCell align="center">{transaction.witnessUsername}</TableCell>
+            <TableCell align="center">{transaction.price}</TableCell>
+            <TableCell align="center">{transaction.category}</TableCell> {/*need to expand to list */}
+        </TableRow>
+        <TableRow>
+            <Collapse in={open}>
+                <div>{transaction.details}</div>
+                <div>{transaction.status}</div>
+            </Collapse>
+        </TableRow>
+    </React.Fragment>
+}
