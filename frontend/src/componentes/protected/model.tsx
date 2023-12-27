26eaac71d81ae93664fbae3e4d1d22d3f9507a8a
shachardavid <shachadavid2013@gmail.com>
2023-12-27 23:06:05 +0200

    alegedly fixed roles and scopes in protected page

--- frontend/src/componentes/protected/model.tsx
+++ frontend/src/componentes/protected/model.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 export interface ProtectedPageParams {
-  level: number
+  reqScope: string
   className?: string
   children: ReactNode
 }
--- frontend/src/componentes/protected/protected-page.tsx
+++ frontend/src/componentes/protected/protected-page.tsx
@@ -1,21 +1,21 @@
 import { useAuth } from '../auth/auth-provider'
+import { Role } from '../auth/models'
 import NotFoundPage from '../not-found-page'
 import { ProtectedPageParams } from './model'
 
 export const ProtectedPage: React.FC<ProtectedPageParams> = ({
-  level,
+  reqScope,
   className,
   children,
 }) => {
-  const { isAutonticated, user, scope, isLoading } = useAuth()
-  return null
+  const { isAutonticated, scope, isLoading } = useAuth()
   if (isLoading) {
     return null
   }
   if (isAutonticated) {
-    // if (level < scope) {
-    //   return null
-    // }
+    if (reqScope === Role.ADMIN && scope !== Role.ADMIN) {
+      return null
+    }
     return <div className={className ? className : ''}>{children}</div>
   }
   return <NotFoundPage />
